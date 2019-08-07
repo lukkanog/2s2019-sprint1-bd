c48597b1076902cbd4ec4ed487a34b5edf4b751c
Lucas Nogueira De Souza <46735197879@info.local>
2019-08-07 10:15:38 -0300

    :hatching_chick:

--- M_SSolve/M_SSolve_dql.sql
+++ M_SSolve/M_SSolve_dql.sql
@@ -9,16 +9,16 @@ SELECT * FROM FuncionariosServicos;
 SELECT S.*,C.*
 	FROM Servicos S
 	INNER JOIN Clientes C
-	ON S.IdCliente = C.IdCliente
+	ON S.IdCliente = C.IdCliente;
 
 SELECT S.*,F.*
 	FROM Servicos S
 	INNER JOIN FuncionariosServicos FS
 	ON S.IdServico = FS.IdServico
 	INNER JOIN Funcionarios F
-	ON FS.IdFuncionario = F.IdFuncionario
+	ON FS.IdFuncionario = F.IdFuncionario;
 
-
+/**/
 
 SELECT S.*,C.RazaoSocial,F.IdFuncionario,F.Nome
 	FROM Servicos S
@@ -27,4 +27,4 @@ SELECT S.*,C.RazaoSocial,F.IdFuncionario,F.Nome
 	INNER JOIN FuncionariosServicos FS
 	ON FS.IdServico = S.IdServico
 	INNER JOIN Funcionarios F
-	ON FS.IdFuncionario = F.IdFuncionario
+	ON FS.IdFuncionario = F.IdFuncionario;
